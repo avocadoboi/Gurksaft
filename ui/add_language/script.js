668526f7af7a40a1d2fdaba036c0d4968a4dc38a
Björn Sundin <bjornsundin02@gmail.com>
2022-10-26 23:10:14 +0200

    Work on ripple and add language logic

--- ui/add_language/script.js
+++ ui/add_language/script.js
@@ -10,16 +10,34 @@ invoke("get_language_list", {}).then((languages) => {
         }
     }
 });
-const translation_languages = [];
+//----------------------------------------------------------------
+function add_language_to_dropdown(language_dropdown, language) {
+    if (!language) {
+        return;
+    }
+    const new_option = document.createElement("option");
+    new_option.value = language;
+    new_option.innerText = language;
+    const options = language_dropdown.options;
+    options.add(new_option, [...options].find(option => option.value > language));
+}
+function remove_language_from_dropdown(language_dropdown, language) {
+    if (!language) {
+        return;
+    }
+    [...language_dropdown.options].find(option => option.value == language)?.remove();
+}
+//----------------------------------------------------------------
 const translation_languages_element = document.getElementById("translation-language-list");
 const translation_language_dropdown = document.getElementById("translation-language");
 document.getElementById("add-translation-language-button")?.addEventListener("click", () => {
     if (translation_language_dropdown.selectedIndex <= 0) {
         return;
     }
+    const translation_language = translation_language_dropdown.value;
     translation_languages_element.innerHTML += `
 		<div>
-			<p>${translation_language_dropdown.value}</p>
+			<p>${translation_language}</p>
 			<button class="red-button remove-translation-language-button">
 				<svg viewBox="0 0 48 48"><path fill="currentColor" d="M9.25 26.3v-4.55h29.5v4.55Z"/></svg>
 			</button>
@@ -27,4 +45,21 @@ document.getElementById("add-translation-language-button")?.addEventListener("cl
 	`;
     translation_language_dropdown.options.remove(translation_language_dropdown.selectedIndex);
     translation_language_dropdown.selectedIndex = 0;
+    remove_language_from_dropdown(target_language_dropdown, translation_language);
+    for (const element of translation_languages_element.children) {
+        element.lastElementChild?.addEventListener("click", () => {
+            const language = element.firstElementChild?.innerHTML;
+            add_language_to_dropdown(translation_language_dropdown, language);
+            add_language_to_dropdown(target_language_dropdown, language);
+            element.remove();
+        });
+    }
+});
+//----------------------------------------------------------------
+const target_language_dropdown = document.getElementById("target-language-dropdown");
+let previous_target_language = "";
+target_language_dropdown.addEventListener("change", () => {
+    add_language_to_dropdown(translation_language_dropdown, previous_target_language);
+    previous_target_language = target_language_dropdown.value;
+    remove_language_from_dropdown(translation_language_dropdown, previous_target_language);
 });
--- ui/common/ui.ts
+++ ui/common/ui.ts
@@ -1,5 +1,61 @@
 
-const ripple_elements = document.getElementsByClassName("ripple");
-for (const ripple_element of ripple_elements) {
+function overlay_color_for_element(element: HTMLElement): string {
+    if (element.classList.contains("overlay-light")) {
+        return "white";
+    }
+    if (element.classList.contains("overlay-accent")) {
+        return getComputedStyle(element).getPropertyValue("--accent");
+    }
+    if (element.classList.contains("overlay-secondary")) {
+        return getComputedStyle(element).getPropertyValue("--secondary");
+    }
+    return "black";
+}
+
+function add_ripple(element: HTMLElement, event: MouseEvent): void {
+    const ripple = document.createElement("div");
+    ripple.className = "ripple-element";
+    ripple.style.position = "absolute";
+    ripple.style.borderRadius = "50%";
+    ripple.style.backgroundColor = overlay_color_for_element(element);
+
+    const width = element.clientWidth;
+    const height = element.clientHeight;
+    const x = event.clientX - element.offsetLeft;
+    const y = event.clientY - element.offsetTop;
     
+    const radius = Math.sqrt(Math.pow(Math.max(x, width - x), 2) + Math.pow(Math.max(y, height - y), 2));
+    ripple.style.width = `${radius*2}px`;
+    ripple.style.height = `${radius*2}px`;
+
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+
+    for (const ripple of element.getElementsByClassName("ripple-element")) {
+        ripple.remove();
+    }
+
+    element.appendChild(ripple);
+}
+
+function fade_ripple(element: HTMLElement): void {
+    for (const ripple of element.getElementsByClassName("ripple-element") as HTMLCollectionOf<HTMLElement>) {
+        ripple.style.opacity = "0";
+    }
+}
+
+const elements_with_ripple = document.getElementsByClassName("ripple") as HTMLCollectionOf<HTMLElement>;
+for (const element of elements_with_ripple) {
+    element.style.overflow = "hidden";
+    element.style.position = "relative";
+    
+    const overlay = document.createElement("div");
+    overlay.className = "hover-overlay";
+
+    element.addEventListener("mouseenter", () => overlay.style.backgroundColor = overlay_color_for_element(element));
+    element.addEventListener("mouseleave", () => overlay.style.backgroundColor = "transparent");
+    element.addEventListener("mousedown", event => add_ripple(element, event));
+    document.addEventListener("mouseup", () => fade_ripple(element));
+
+    element.appendChild(overlay);
 }
